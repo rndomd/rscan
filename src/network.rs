9308337a3d0067bf77bd0305a22197d134d6ed81
Random <fcruz170@gmail.com>
2026-09-10 12:33:44 +0100

    clean up old rtm_getaddr logic

--- src/network.rs
+++ src/network.rs
@@ -1,16 +1,13 @@
 use libc::{
-    AF_INET, AF_INET6, AF_NETLINK, AF_UNSPEC, IFA_LOCAL, IFNAMSIZ, IPPROTO_ICMP, NETLINK_ROUTE,
-    NLM_F_DUMP, NLM_F_REQUEST, NLMSG_DONE, RTM_GETADDR, SO_RCVTIMEO, SOCK_DGRAM, SOCK_RAW,
-    SOCK_STREAM, SOL_SOCKET, addrinfo, bind, freeaddrinfo, freeifaddrs, getaddrinfo, gethostname,
-    getifaddrs, getpid, if_indextoname, ifaddrmsg, ifaddrs, nlmsghdr, recv, recvfrom, rtattr, send,
-    sendto, setsockopt, sockaddr, sockaddr_in, sockaddr_nl, sockaddr_storage, socket, socklen_t,
-    suseconds_t, time_t, timeval,
+    AF_INET, AF_NETLINK, AF_UNSPEC, IPPROTO_ICMP, SO_RCVTIMEO, SOCK_DGRAM, SOCK_STREAM, SOL_SOCKET,
+    addrinfo, bind, freeaddrinfo, freeifaddrs, getaddrinfo, gethostname, getifaddrs, getpid,
+    ifaddrs, recvfrom, sendto, setsockopt, sockaddr, sockaddr_in, sockaddr_nl, sockaddr_storage,
+    socket, socklen_t, suseconds_t, time_t, timeval,
 };
 use std::{
-    collections::HashMap,
     ffi::{CStr, c_char},
     mem,
-    net::{IpAddr, Ipv4Addr, Ipv6Addr},
+    net::{IpAddr, Ipv4Addr},
     os::{
         fd::{AsRawFd, FromRawFd, OwnedFd, RawFd},
         raw::{c_int, c_void},
@@ -38,146 +35,6 @@ pub fn ping_local_ip(ip: Ipv4Addr) -> Result<Option<Ipv4Addr>, anyhow::Error> {
     let imsg = create_icmp_ping_message();
     send_ping(sockfd.as_raw_fd(), imsg, ip).context("failed to send icmp message")?;
     recv_ping(sockfd.as_raw_fd()).context("failed to retrieve ping reply")
-}
-
-//fn parse_rtaattr_data(
-//    addr_msg: &ifaddrmsg,
-//    buf: &[u8],
-//    data_offset: &mut usize,
-//    msg_end: usize,
-//) -> Option<NetworkInterface> {
-//    let mut found: bool = false;
-//    while *data_offset + mem::size_of::<rtattr>() <= msg_end {
-//        let rta = unsafe { &*(buf[*data_offset..].as_ptr() as *const rtattr) };
-//        let attr_len = rta.rta_len as usize;
-//        if attr_len < mem::size_of::<rtattr>() || *data_offset + attr_len > msg_end {
-//            break;
-//        }
-//        let attr_data_start = *data_offset + mem::size_of::<rtattr>();
-//        let attr_data_end = *data_offset + attr_len;
-//        let attr_data = &buf[attr_data_start..attr_data_end];
-//
-//        match rta.rta_type {
-//            IFA_LOCAL => match addr_msg.ifa_family as i32 {
-//                AF_INET => {
-//                    let addr = IpAddr::V4(Ipv4Addr::new(
-//                        attr_data[0],
-//                        attr_data[1],
-//                        attr_data[2],
-//                        attr_data[3],
-//                    ));
-//                    if !addr.is_loopback() {
-//                        ntw_if.add_subnet(Subnet::new(addr, addr_msg.ifa_prefixlen));
-//                        found = true;
-//                    }
-//                }
-//                AF_INET6 => {
-//                    let bytes: [u8; 16] = attr_data[..16].try_into().unwrap();
-//                    let addr = IpAddr::V6(Ipv6Addr::from(bytes));
-//                    if !addr.is_loopback() {
-//                        ntw_if.add_subnet(Subnet::new(addr, addr_msg.ifa_prefixlen));
-//                        found = true;
-//                    }
-//                }
-//                _ => {}
-//            },
-//            _ => {}
-//        }
-//        *data_offset += (attr_len + 3) & !3;
-//    }
-//
-//    if found {
-//        let ifindex = u32::from_ne_bytes(addr_msg.ifa_index.to_ne_bytes());
-//        let mut name = [0 as c_char; IFNAMSIZ];
-//        unsafe {
-//            if_indextoname(ifindex, name.as_mut_ptr());
-//        }
-//        let name = unsafe { CStr::from_ptr(name.as_ptr()) };
-//        ntw_if.set_name(&name.to_string_lossy());
-//
-//        return Some(ntw_if);
-//    } else {
-//        return None;
-//    }
-//}
-//
-//fn recv_rtmsg(fd: RawFd) -> Result<NetworkInterface, DiscoverError> {
-//    let mut buf = [0u8; 8192];
-//    loop {
-//        let received = unsafe { recv(fd, buf.as_mut_ptr() as *mut c_void, buf.len(), 0) };
-//        if received < 0 {
-//            return Err(DiscoverError::SocketError {
-//                source: std::io::Error::last_os_error(),
-//            });
-//        }
-//        if received == 0 {
-//            break;
-//        }
-//
-//        let mut offset = 0usize;
-//
-//        while offset < received as usize {
-//            let hdr = unsafe { &*(buf[offset..].as_ptr() as *const nlmsghdr) };
-//            if hdr.nlmsg_type == NLMSG_DONE as u16 {
-//                return Ok(interfaces);
-//            }
-//            let msg_len = hdr.nlmsg_len as usize;
-//            if msg_len < mem::size_of::<nlmsghdr>() || offset + msg_len > buf.len() {
-//                break;
-//            }
-//
-//            let msg_end = offset + msg_len;
-//            let attrs_offset = offset + mem::size_of::<nlmsghdr>();
-//            let mut data_offset = attrs_offset + mem::size_of::<ifaddrmsg>();
-//            if data_offset > msg_end {
-//                break;
-//            }
-//            let addr_msg = unsafe { &*(buf[attrs_offset..].as_ptr() as *const ifaddrmsg) };
-//            if let Some(iface) = parse_rtaattr_data(addr_msg, &buf, &mut data_offset, msg_end) {
-//                return Ok(iface);
-//            }
-//            offset += (msg_len + 3) & !3;
-//        }
-//    }
-//    Err(DiscoverError::NetworkInterfaceNotFound {
-//        iface: "local".to_string(),
-//    })
-//}
-
-fn send_rtmsg(fd: RawFd, addr_msg: ifaddrmsg, nlh: nlmsghdr) -> Result<(), DiscoverError> {
-    unsafe {
-        let mut buf = [0u8; size_of::<nlmsghdr>() + size_of::<ifaddrmsg>()];
-        std::ptr::copy_nonoverlapping(
-            &nlh as *const _ as *const u8,
-            buf.as_mut_ptr(),
-            size_of::<nlmsghdr>(),
-        );
-        std::ptr::copy_nonoverlapping(
-            &addr_msg as *const _ as *const u8,
-            buf.as_mut_ptr().add(mem::size_of::<nlmsghdr>()),
-            size_of::<ifaddrmsg>(),
-        );
-        let sent = send(fd, buf.as_ptr() as *const c_void, buf.len(), 0);
-        if sent < 0 {
-            return Err(DiscoverError::SendMessageError {
-                sock_type: String::from("RTM"),
-                source: std::io::Error::last_os_error(),
-            });
-        }
-        Ok(())
-    }
-}
-
-fn build_rtm_getaddr() -> (ifaddrmsg, nlmsghdr) {
-    let mut addr_msg: ifaddrmsg = unsafe { mem::zeroed() };
-    addr_msg.ifa_family = AF_UNSPEC as u8;
-    let mut nlh: nlmsghdr = unsafe { mem::zeroed() };
-    nlh.nlmsg_len = (size_of::<nlmsghdr>() + size_of::<ifaddrmsg>()) as u32;
-    nlh.nlmsg_type = RTM_GETADDR;
-    nlh.nlmsg_flags = (NLM_F_REQUEST | NLM_F_DUMP) as u16;
-    nlh.nlmsg_seq = 1;
-
-    (addr_msg, nlh)
 }
 
 fn open_socket(domain: c_int, sock_type: c_int, protocol: c_int) -> Result<OwnedFd, DiscoverError> {
